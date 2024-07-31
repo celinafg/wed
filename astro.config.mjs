6cecabfca84e88769a5d13a8947fa8b81b8f1fa7
celina garcia <code@koral.one>
2024-07-30 20:21:06 -0700

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
-import db from "@astrojs/db";
-import vercel from "@astrojs/vercel/serverless";
 
 import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-naut-template.vercel.app/",
   output: "server",
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "standalone",
+  }),
 });
